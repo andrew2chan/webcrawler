b4711251b0cc717951a0281216da851d7ecde269
Andrew Chan <chan.andrew1995@gmail.com>
2025-03-01 12:16:34 -0500

    first commit

---
+++ src/test/java/com/andrew2chan/webcrawler/WebcrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrew2chan.webcrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebcrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
